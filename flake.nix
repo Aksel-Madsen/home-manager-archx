39c0b818d28f9c5a155203675783c3cf65aa48da
Aksel Madsen <aksel.m.madsen@outlook.com>
2026-02-28 13:20:26 +0100

    bugfixing

--- flake.nix
+++ flake.nix
@@ -1,27 +1,26 @@
 {
-  description = "MyHomeManager";
+  description = "Home manager config";
 
   inputs = {
-
     nixpkgs.url = "nixpkgs/nixos-25.11";
 
     home-manager = {
-    url = "github:nix-community/home-manager/release-25.11";
-    inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = {nixpkgs, home-manager, ...}:
-  let
-    lib = nixpkgs.lib;
-    system = "x86_64-linix";
-    pkgs = import nixpkgs { inherit system; }
-  in {
-    homeConfigurations = {
-      archo = home-manager.lib.homeManager.Configuration {
-        inherit pkgs;
-	modules = [ ./home.nix ];
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      homeConfigurations = {
+        archo = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home.nix ];
+        };
       };
     };
-  };
 }
