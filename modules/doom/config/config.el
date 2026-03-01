6c5d8d5c45f0009d5bb1149a5b172db705f24d05
Aksel Madsen <aksel.m.madsen@outlook.com>
2026-03-01 13:36:07 +0100

    Font size adjustment for larger screen on Archo

--- modules/doom/config/config.el
+++ modules/doom/config/config.el
@@ -21,8 +21,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "Source Code Pro" :size 35)
-     doom-variable-pitch-font (font-spec :family "Source Code Pro" :size 35))
+(setq doom-font (font-spec :family "Source Code Pro" :size 40)
+     doom-variable-pitch-font (font-spec :family "Source Code Pro" :size 40))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
